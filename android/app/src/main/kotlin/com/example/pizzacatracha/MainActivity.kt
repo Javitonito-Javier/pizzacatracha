57eb8829d48a9b34980b18b60b28949de415a98d
Javier-Javitonito <jkmeza92@gmail.com>
2021-01-20 20:27:55 -0600

    probando 1

---
+++ android/app/src/main/kotlin/com/example/pizzacatracha/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pizzacatracha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/login.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoveThis extends StatefulWidget {
+  @override
+  _LoveThisState createState() => _LoveThisState();
+}
+
+class _LoveThisState extends State<LoveThis> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
